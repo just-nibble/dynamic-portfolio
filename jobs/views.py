0dbc8f156635f0ee38d445928f96a9f95775ddeb
princewill <princewillolaiya@gmail.com>
2021-01-27 22:59:37 +0100

    added homepage view

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+
+def homepage(request):
+    job_list = Job.objects.all()
+    return render(request, 'index.html', {"jobs": job_list})
