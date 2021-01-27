53085b44dd3f9ead24c3d41fa8fef51ac3e11367
princewill <princewillolaiya@gmail.com>
2021-01-27 22:52:43 +0100

    Added cloudinary image to job app

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Job(models.Model):
     job_name = models.CharField(max_length=100)
     job_link = models.URLField(null=True, blank=True)
     job_repo = models.URLField(null=True, blank=True)
+    job_image = CloudinaryField("job_image", blank=True, null=True)
     job_description = models.CharField(max_length=100)
     technologies_used = models.CharField(max_length=200)
 
