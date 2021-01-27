1bebd765f87259b3633852ccb80dcd354bec570f
princewill <princewillolaiya@gmail.com>
2021-01-27 22:43:51 +0100

    created jobs app to record the jobs I complete

---
+++ jobs/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Job(models.Model):
+    job_name = models.CharField(max_length=100)
+    job_link = models.URLField(null=True, blank=True)
+    job_repo = models.URLField(null=True, blank=True)
+    job_description = models.CharField(max_length=100)
+    technologies_used = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.job_name)
+
+    class Meta:
+        ordering = "job_name"
