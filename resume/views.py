9f63c2859d1e0d11d1f41837244ccaa65eaa08b4
princewill <princewillolaiya@gmail.com>
2021-02-22 20:20:15 +0100

    added first views for resume

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Education, Experience, Skill, Hobby
 # Create your views here.
+
+
+def resume(request):
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+    skill = Skill.objects.all()
+    hobby = Hobby.objects.all()
+    content = {
+        'education': education,
+        'experience' experience,
+        'skill': skill,
+        'hobby': hobby,
+    }
+    return render(
+        request, "resume.html", content,
+    )
