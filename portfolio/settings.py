df748f0ac3e191d2bd574109f5f29de07b2e2f47
princewill <princewillolaiya@gmail.com>
2021-02-25 23:35:35 +0100

    made adding jobs dynamic

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import cloudinary
-import cloudinary.api
-import cloudinary.uploader
-from . secret import SECRET_KEY
+
+from . secret import SECRET_KEY, CLOUDINARY_CONFIG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
